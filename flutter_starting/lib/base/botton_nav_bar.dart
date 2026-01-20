0cfb72f5ca62ece7bccb44a8cb534ced3344836d
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-01-20 17:44:39 +0530

    add the icons and make the bottom nav bar

--- flutter_starting/lib/base/botton_nav_bar.dart
+++ flutter_starting/lib/base/botton_nav_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 
 class BottonNavBar extends StatefulWidget {
   const BottonNavBar({super.key});
@@ -14,7 +15,20 @@ class _BottonNavBarState extends State<BottonNavBar> {
     body: Center(
       child: Text("Ticket Info"),
     ),
-    bottomNavigationBar: Container(child: Text("Botton"),),
+    bottomNavigationBar:  BottomNavigationBar(
+      selectedItemColor: Colors.blueGrey,
+      unselectedItemColor: const Color(0xFF526400),
+      showSelectedLabels: false,
+
+      items: [
+        BottomNavigationBarItem(icon:Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+        activeIcon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+        label: "Home"),
+        BottomNavigationBarItem(icon:Icon(Icons.search),label: "Search" ),
+        BottomNavigationBarItem(icon:Icon(Icons.airplane_ticket),label: "Tickets" ),
+        BottomNavigationBarItem(icon:Icon(Icons.person),label: "Profile" ),
+       
+    ]),
     );
 
   }
