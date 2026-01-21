6eab68d5237e8efdfb62f4391bc1e84b8318d1fd
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-01-22 00:15:16 +0530

    addthe image add make it header

--- flutter_starting/lib/screens/home_screen.dart
+++ flutter_starting/lib/screens/home_screen.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starting/base/res/styles/app_styles.dart';
 
-class  HomeScreen extends StatelessWidget {
-  const  HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      body:ListView(
+    return Scaffold(
+      body: ListView(
         children: [
           Container(
-            child: Text("Hello home screen"),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        Text("Good morning", style: AppStyles.headLineStyle2),
+                        SizedBox(height: 5),
+                        Text("Books Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.amberAccent,
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image:AssetImage("assets/images/logo.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Serach icon"), Text("Empy space")],
+                ),
+              ],
+            ),
           ),
-          Container(
-            child: Text("How are you ?"),
-          )
         ],
-      )
+      ),
     );
   }
 }
